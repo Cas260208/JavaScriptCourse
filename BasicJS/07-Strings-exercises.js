934afa580de36e50feeb5680446551f3e9320865
Cas260208 <caszero.260208@gmail.com>
2025-03-30 01:58:32 -0600

    Ejercios extra

--- BasicJS/07-Strings-exercises.js
+++ BasicJS/07-Strings-exercises.js
@@ -1,19 +1,35 @@
 // 1. Concatena dos cadenas de texto
+let cad1 = "años";
+let cad2 = "Iván Garrido";
+let cadena = "El nombre de mi novio es: " + cad2 + " y llevo 3 " + cad1 + ".";
 
 // 2. Muestra la longitud de una cadenad e texto
+console.log(cadena.length);
 
 // 3. Muestra el primer y el último carácter de un string
+console.log(cadena[0]);
+console.log(cadena.length - 1); 
 
 // 4. Convierte a mayúsculas y minúsculas un string
+console.log(cadena.toUpperCase());
+console.log(cadena.toLowerCase());
 
 // 5. Crea una cadena de texto en varias líneas
+let multiString = `Hola, soy 
+Casandra y tengo 22 años, estudio en la 
+UAM Cuajimalpa.`
 
-// 6. Interpola el valor de una variable en un string 
+// 6. Interpola el valor de una variable en un string
+console.log(`Hola, soy ${cad2} y tengo 24 años.`); 
 
-// 7. Reemplaza todos los espacios en balndo de un string por guiones 
+// 7. Reemplaza todos los espacios en blaco de un string por guiones 
+console.log(cadena.replace(/ /g, "-")); // Cambia todos los espacios por guiones 
 
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+console.log(cadena.includes("Iván"));
 
 // 9. Comprueba si dos string son iguales 
+console.log(cad1 === cad2); // false porque son diferentes
 
 // 10. Comprueba si dos strings tienen la misma longitud 
+console.log(cad1.length == cad2.length); // false porque son diferentes
---
+++ BasicJS/08-Condionals.js
@@ -0,0 +1,31 @@
+// Condicionales
+
+// if, else if, else 
+
+//if (si)
+let age = 23;
+
+if (age == 22) {
+    // Bloque 
+    console.log("La edad es 22");
+}
+
+// else (si no)
+if (age == 22) {
+    // Bloque 
+    console.log("La edad es 22");
+} else {
+    console.log("La edad no es 22");
+}
+
+// else if (si no, si)
+if (age == 22) {
+    // Bloque 
+    console.log("La edad es 22");
+} else if (age < 18) {
+    console.log("Es menor de edad");
+} else {
+    console.log("La edad no es 22 y no es menor de edad");
+}
+
+// Operador ternario
